72357dcfb9d53170cb27638e7b7ccfe5c6e618f1
AminulIslamorange <aminulislamorange@gmail.com>
2025-10-10 20:21:29 +0600

    marquee fixed

--- src/Pages/Home/ClientLogosMarquee/ClientLogosMarquee.jsx
+++ src/Pages/Home/ClientLogosMarquee/ClientLogosMarquee.jsx
@@ -1,4 +1,3 @@
-
 import logo1 from '../../../assets/brands/casio.png'
 import logo2 from '../../../assets/brands/amazon.png'
 import logo3 from '../../../assets/brands/amazon_vector.png'
@@ -8,46 +7,48 @@ import logo6 from '../../../assets/brands/start-people 1.png'
 import logo7 from '../../../assets/brands/start.png'
 import Marquee from 'react-fast-marquee'
     
-    const ClientLogosMarquee= () => {
-        const logos = [
-    logo1,
-    logo2,
-    logo3,
-    logo4,
-    logo5,
-    logo6,
-    logo7
-  ];
-        return (
-            <div className="py-10 bg-white rounded-[24px] overflow-hidden">
-                <Marquee
-                    speed={50}
-                    direction="left"
-                    gradient={false}
-                    pauseOnHover={true}  
-                >
-                    {logos.map((logo, index) => (
-                        <div
-                            key={index}
-                            className="mx-10 transition-transform duration-300 hover:scale-110"
-                        >
-                            <img
-                                src={logo}
-                                alt={`Company ${index + 1}`}
-                                className="
-                w-32 h-16 
-                object-contain 
-                grayscale 
-                hover:grayscale-0 
-                transition-all 
-                duration-300
-              "
-                            />
-                        </div>
-                    ))}
-                </Marquee>
-            </div>
-        );
-    };
+const ClientLogosMarquee = () => {
+    const logos = [
+        logo1,
+        logo2,
+        logo3,
+        logo4,
+        logo5,
+        logo6,
+        logo7
+    ];
 
-    export default ClientLogosMarquee;
+    return (
+        <div className="py-10 bg-white rounded-[24px] overflow-hidden">
+            <Marquee
+                speed={50}
+                direction="left"
+                gradient={false}
+                pauseOnHover={true}  
+            >
+                {logos.map((logo, index) => (
+                    <div
+                        key={index}
+                        className="mx-10 transition-transform duration-300 hover:scale-110"
+                    >
+                        <img
+                            src={logo}
+                            alt={`Company ${index + 1}`}
+                            className="object-contain grayscale hover:grayscale-0 transition-all duration-300"
+                            style={{
+                                width: "123px",
+                                height: "24px",
+                                aspectRatio: "41/8"
+                            }}
+                        />
+                    </div>
+                ))}
+            </Marquee>
+            <div>
+                <hr className="border-t-2 border-dashed border-[#03464D] my-8" />
+            </div>
+        </div>
+    );
+};
+
+export default ClientLogosMarquee;
