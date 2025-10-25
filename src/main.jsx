1c3efc0860df7fe8b7ed37867de8aab5714e947f
AminulIslamorange <aminulislamorange@gmail.com>
2025-10-25 19:58:32 +0600

    tanstack query added

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,18 @@ import { router } from './Routes/Routes.jsx';
 import 'aos/dist/aos.css';
 import Aos from 'aos';
 import AuthProvider from './context/AuthContext/AuthProvider.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 Aos.init();
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <div className='max-w-7xl mx-auto font-urbanist'>
-     <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+       <AuthProvider>
       <RouterProvider router={router} />
       </AuthProvider>
+    </QueryClientProvider>
    </div>
   </StrictMode>,
 )
